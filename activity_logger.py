f5e2b657be036443d5341e96bfc3d6ae256bd638
r0han99 <r409067@gmail.com>
2022-02-13 22:20:28 +0530

    V1.0 complete - setup.sh pending

--- activity_logger.py
+++ activity_logger.py
@@ -1,13 +1,15 @@
 #!/Users/rohan/opt/anaconda3/bin/python
 
+import shutil
 import sys 
 import os
 import argparse
 import numpy as np
 import pandas as pd
-import datetime
+from datetime import datetime
+from zipfile import ZipFile
+from tqdm import tqdm
 
-from sqlalchemy import desc
 
 
 # SCRIPT VERSION
@@ -19,13 +21,21 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--init", help='Initialise Setup.', action='store_true')
 parser.add_argument("-t", "--tasks", type=str, help='Enter all the key-topics formatted with hypens("-").')
 parser.add_argument("-i", "--info", type=str, help="Input Y/N to enter additional details in the catalog.", choices=['Y','N','y','n'])
+parser.add_argument("--summary", help='Summary of the most recent work', action='store_true')
 parser.add_argument("--backup", type=str, help='backup.',choices=['partial','absolute', 'par','abs'])
-parser.add_argument('--version', action='version', version=f'- %(prog)s (V{VERSION})')
+parser.add_argument('--version', action='version', version=f'Activity Logger (actlogger) \033[96m(V{VERSION})\033[0m')
                     
 args = parser.parse_args()
 
+# print(args.init)
+# print(args.info)
+# print(args.tasks)
+# print(args.backup)
+
+
+
 abspath = os.getcwd()
-path = os.path.join(abspath, 'archive')
+path = os.path.join(abspath, '.archive')
 
 
 # INIT
@@ -69,6 +79,14 @@ def init():
             os.mkdir(os.path.join(path, 'details'))
             print('+ Additional content repository established.')
 
+        # backup folder
+        if os.path.exists(os.path.join(path, 'backup')):
+            print('- Backup repository exists.')
+        else:
+            print("x Backup repository doesn't exist")
+            os.mkdir(os.path.join(path, 'backup'))
+            print('+ Backup repository established.')
+
             
         if args.init == True:
             print('Setup Complete, use activity_logger --help for functionality and assistance.')
@@ -100,16 +118,23 @@ def init():
         os.mkdir(os.path.join(path, 'details'))
         print('+ Additional content repository established.')
 
+         # backup folder
+        os.mkdir(os.path.join(path, 'backup'))
+        print('+ Backup repository established.')
+
         if args.init == True:
             print('Setup Complete, use activity_logger --help for functionality and assistance.')
         else:
             print('Setup Complete.')
+
+            
         print('')
 
 
 def fetch_records():
+
     global path 
-    if os.path.exists(path):
+    if os.path.exists(os.path.join(path,'logdata.csv')):
 
         # Global Day Counter : Initial 
         try:
@@ -121,13 +146,20 @@ def fetch_records():
         except:
             day = 0
 
+        # secondary check 
+        if 'Unnamed: 0' in records.columns:
+            records = records.drop('Unnamed: 0',axis=1)
+        
+
         return records, day
     else:
 
         print('x Missing required archive, executing --init to establish files.')
         init()
+        print('*' * 25)
         records, day = fetch_records()
         return records, day
+
 
 
 
@@ -153,7 +185,7 @@ def package_details(tasks, info='N'):
 
     global path
     _, day = fetch_records()
-    date = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S") 
+    date = datetime.now().strftime("%Y-%m-%d-%H:%M:%S") 
     day = day+1
     idnum = idgen(os.path.join(path,'logdata.csv'))
     task = '; '.join(list(map(str.capitalize, args.tasks.split('-'))))
@@ -166,7 +198,9 @@ def package_details(tasks, info='N'):
 
 
 def write_data(packet,records,template):
+
     global path
+    
     # Write Data into DataFrame
     row = list(packet.values())
     # print(row)
@@ -197,7 +231,7 @@ def backup_suggestion(day):
 def silent_check():
 
     abspath = os.getcwd()
-    path = os.path.join(abspath, '/archive')
+    path = os.path.join(abspath, '.archive')
     
     # archive existence check
     if os.path.exists(path):
@@ -220,6 +254,8 @@ def silent_check():
                 Write a functionality or an addtional flag prompt to synchronise the day counter.
             
             '''
+            print('-- Proceeding')
+            print('')
             
             
 
@@ -233,8 +269,10 @@ def silent_check():
             with open(os.path.join(path, 'tasksequence.log'), 'w') as f:
                 pass
             print("+ Task Sequence LogFile established.")
+        
+            print('-- Proceeding')
+            print('')
 
-            
         # details folder
         if os.path.exists(os.path.join(path, 'details')):
             # print('- Additional content repository exists.')
@@ -245,8 +283,27 @@ def silent_check():
             print('+ Additional content repository established.')
 
             
-        print('-- Proceeding')
-        print('')
+            print('-- Proceeding')
+            print('')
+
+        # backup folder
+        if os.path.exists(os.path.join(path, 'backup')):
+            # print('- Additional content repository exists.')
+            pass
+        else:
+            print("x \033[91m[FATAL]\033[0m Backup repository doesn't exist")
+            os.mkdir(os.path.join(path, 'backup'))
+            print('+ Backup repository established.')
+
+            
+            print('-- Proceeding')
+            print('')
+
+        '''
+        Display a Note to inform the user that if at all after the FATAL prompt the data logged is not the first, then in that case
+        user should be prompted to use --restore (V.2)
+        
+        '''
         
         
 ## ARGUMENT PROCESSING
@@ -256,6 +313,7 @@ try:
     funcs['init'] = args.init
     funcs['tasks'] = args.tasks
     funcs['info'] = args.info
+    funcs['summary'] = args.summary
     funcs['backup'] = args.backup
     funcs['version'] = args.version
 except:
@@ -268,14 +326,15 @@ if not funcs['tasks'] == None:
     # silent check for all the requirced file's existence
     silent_check()
 
-    
-
+    # fetching records
     records, day = fetch_records()
     packet, template, display_template = package_details(args.tasks, args.info)
     DAY = packet['DAY']
 
     #backup prompt check
     backup_suggestion(DAY)
+
+    # print logged information
     print('+',display_template, end='--')
     
     packet['DETAILS'] = 'n'
@@ -291,7 +350,8 @@ if not funcs['tasks'] == None:
             print(f'-- Additional Content [{idnum}]-- ')
             content = input('Enter content to be entered: ')
             print(f'File Created - {idnum}.txt')
-            with open(f'./archive/details/{idnum}.txt','w') as f:
+            
+            with open(os.path.join(path, f'details/{idnum}.txt'),'w') as f:
                 f.write(content)
             
             print('+ Addtional content',end='')
@@ -300,21 +360,68 @@ if not funcs['tasks'] == None:
 
 elif not funcs['backup'] == None:
 
-    # check if the logs are absolutely empty which doesn't require any backup procedure
-    # check if the logfile and the logdatabase have atleast 20 Logs else prompt to override the backup procedure
-
-
-    if args.backup == 'partial':
-
-        print('Partial Backup')
-        
+    # check if the logs are absolutely empt
+    # check if the logfile and the logdatabase have atleast 20 Logs else prompt to override the backy which doesn't require any backup procedureup procedure
     
-    elif args.backup == 'absolute':
 
-        print('Absolute Backup')
+    # to make sure everything's in place
+    silent_check()
 
-    else:
-        print('Invalid Backup type')
+    records, _ = fetch_records()
+    control = 'proceed'
+
+    # initial checks
+    if len(records) == 0:
+        print('x No Records exists to be backed up.')
+        control = 'halt'
+    elif len(records) <= 20:
+        print('~ Logs are limited, not in the optimal backup range. ')
+        while True:
+            flag = input('\nEnter `override` or `stop` to control : ')
+            if flag == 'override':
+                print('+ overriding backup, mode = ', end='')
+                control = 'proceed'
+                break
+            elif flag == 'stop':
+                print('stopping backup')
+                control = 'halt'
+                break
+            else:
+                print('x Invalid control choice!')
+                
+        
+    if control == 'proceed':
+    
+        if args.backup == 'partial':
+            # mode
+            print('Partial')
+            logdatapath = os.path.join(path, 'logdata.csv')
+            tasksequencepath = os.path.join(path, 'tasksequence.log')
+
+            
+            # Generating Filename -- Time Delta
+            dt = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+            dt = dt.replace('.','x')
+            dt = '_'.join(dt.split(' '))
+
+
+            # zipping
+            tqdmprompts = [logdatapath,tasksequencepath]
+            arcnames = ['logdata.csv','tasksequence.csv']
+            cprname = dt+'.zip'
+            with ZipFile(os.path.join(path,cprname), 'w') as zipper:
+                for cprfile in tqdm(tqdmprompts, desc='compressing'):
+                    zipper.write(cprfile, arcname=cprfile.split('/')[-1:][0])
+                    
+            # move the generated zip into the backup folder
+            shutil.move(os.path.join(path,cprname),os.path.join(path,'backup/'))
+
+        
+        elif args.backup == 'absolute':
+            print('Absolute Backup')
+            print('- under development')
+
+       
 
 
 elif args.init:
@@ -322,6 +429,31 @@ elif args.init:
     # check if the archive/ logdata.csv and tasksequence.log exists; if Yes then terminate else establish
     init()
 
+elif args.summary:
+
+    # fetch records
+    records, day = fetch_records()
+    
+    if len(records) != 0:
+        print('+ \033[96mSummary\033[0m')
+        print(f'- Streak - Days: [{day}]')
+        print(f"- TID - ")
+        print(f"- Most Recent Work - [{records['TID'].values[-1:][0]}] : [{records['TASK'].values[-1:][0]}]")
+        print(f"- Date Logged- [{records['DATE'].values[-1:][0]}]")
+
+        '''
+        Additional Info exists print that 
+        '''
+
+
+    else:
+        print('x No Records to summarise.')
+
+elif not args.info == None:
+    print('x [invalid usage] --info is used in conjuction to --tasks. try --help for assistance.')
+
+elif args.init == False and args.backup == None and args.info == None and args.tasks == None:
+    print('x Try seeking assitance with --help.') 
 
 
     
